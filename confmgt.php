bb91624acaba4a7a67026558c15b737fa21ee09c
Kaushal Keraminiyage <admin@confmgt.com>
2013-07-29 23:16:25 +0100

    Initial Commit

--- confmgt.php
+++ confmgt.php
@@ -1,9 +1,5 @@
 <?php
-/**                               ______________________________________________
-*                          o O   |                                              |
-*                 (((((  o      <    Generated with Cook Self Service  V2.5.6   |
-*                ( o o )         |______________________________________________|
-* --------oOOO-----(_)-----OOOo---------------------------------- www.j-cook.pro --- +
+/**     
 * @version		0.3.1.2
 * @package		Confmgt
 * @subpackage	
@@ -11,15 +7,7 @@
 * @author		Dr Kaushal Keraminiyage - www.confmgt.com - admin@confmgt.com
 * @license		GNU/GPL
 *
-* /!\  Joomla! is free software.
-* This version may have been modified pursuant to the GNU General Public License,
-* and as distributed it includes or is derivative of works licensed under the
-* GNU General Public License or other free or open source software licenses.
 *
-*             .oooO  Oooo.     See COPYRIGHT.php for copyright notices and details.
-*             (   )  (   )
-* -------------\ (----) /----------------------------------------------------------- +
-*               \_)  (_/
 */
 
 
