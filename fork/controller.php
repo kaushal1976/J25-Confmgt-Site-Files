94d48f12c2b5075add8d1a09f095e94501fd75e9
Kaushal Keraminiyage <admin@confmgt.com>
2013-07-31 00:11:08 +0100

    Correction
    Code correction for the authorlogin layout redirection message ($msg)

--- fork/controller.php
+++ fork/controller.php
@@ -1,25 +1,11 @@
 <?php
-/**                               ______________________________________________
-*                          o O   |                                              |
-*                 (((((  o      <    Generated with Cook Self Service  V2.5.6   |
-*                ( o o )         |______________________________________________|
-* --------oOOO-----(_)-----OOOo---------------------------------- www.j-cook.pro --- +
-* @version		0.3.1.2
+/**                     
 * @package		Confmgt
 * @subpackage	
 * @copyright	Copyright 2013, All rights reserved
 * @author		Dr Kaushal Keraminiyage - www.confmgt.com - admin@confmgt.com
 * @license		GNU/GPL
 *
-* /!\  Joomla! is free software.
-* This version may have been modified pursuant to the GNU General Public License,
-* and as distributed it includes or is derivative of works licensed under the
-* GNU General Public License or other free or open source software licenses.
-*
-*             .oooO  Oooo.     See COPYRIGHT.php for copyright notices and details.
-*             (   )  (   )
-* -------------\ (----) /----------------------------------------------------------- +
-*               \_)  (_/
 */
 
 // no direct access
@@ -63,10 +49,10 @@ class ConfmgtController extends ConfmgtCkController
 		if (!$layouttest == 'authorlogin') {
 		
 			$user = JFactory::getUser();
-			$msg = JText::_('CONFMGT_AUTHLOGIN_MSG');
+			$msg = JText::_('CONFMGT_LAYOUT_AUTHLOGIN_MSG');
 			if ($user->get('guest') == 1) {
 				// Redirect to login page.
-			$this->setRedirect(JRoute::_('index.php?option=com_confmgt&view=authorsitem&layout=authorlogin', $msg));
+			$this->setRedirect(JRoute::_('index.php?option=com_confmgt&view=authorsitem&layout=authorlogin'),$msg,'message');
 			return;
 			}
 		}
